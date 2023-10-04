0f50f3058a4c8eaa5828b599a680771f8c144bf9
aksaddleback <142381968+aksaddleback@users.noreply.github.com>
2023-10-04 04:53:13 +0000

    more loops

---
+++ 3-loops/program7.cpp
@@ -0,0 +1,13 @@
+//Develop a program that uses a do-while loop to keep asking 
+//the user for a password until they enter the correct one.
+#include<iostream>
+using namespace std; 
+int main() { 
+    string correctPass = "c++";
+    string enteredPass; 
+    do { 
+        cout << "Please enter the password:"; 
+        cin >> enteredPass;
+    } while(enteredPass != correctPass);
+    cout << "you enered the coorect password. good job!" << "\n";
+}
